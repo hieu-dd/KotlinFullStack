4f3f2cbd9e8523908195de5d96577254c98bc60f
14H <hieu.dd@teko.vn>
2021-06-22 16:02:15 +0700

    update domain kmm

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,21 @@
 allprojects {
     repositories {
-        mavenCentral()
+        gradlePluginPortal()
+        jcenter()
         google()
-        maven(url = "https://jitpack.io")
+        mavenCentral()
     }
 }
-buildscript {
 
+buildscript {
     repositories {
+        gradlePluginPortal()
+        jcenter()
         google()
         mavenCentral()
-        maven(url = "https://maven.fabric.io/public")
     }
     dependencies {
-        classpath ("com.android.tools.build:gradle:4.0.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+        classpath("com.android.tools.build:gradle:4.0.1")
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "KotlinTest"
 include(":ui")
+include("domain")
